132bc855d808b23694fa0d4d1be6331c840d8a6d
chitrakar <lasata16@gmail.com>
2020-12-14 20:18:26 +0545

    Added Department

--- EmployeeManagement/Controllers/DepartmentController.cs
+++ EmployeeManagement/Controllers/DepartmentController.cs
@@ -6,7 +6,6 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using EmployeeManagement.Models;
-
 namespace EmployeeManagement.Controllers
 {
     public class DepartmentController : Controller
@@ -16,18 +15,31 @@ namespace EmployeeManagement.Controllers
             Department department1 = new Department()
             {
                 Id = 1,
-                DepartmentName = "IT",
-                Salary = 1546312
+                DepartmentName = "Manager",
+                Salary = 75000
             };
             Department department2 = new Department()
             {
                 Id = 2,
                 DepartmentName = "Developer",
-                Salary = 6312155
+                Salary = 50000
             };
             List<Department> departments =  new List<Department>() {department1, department2};
 
             return View(departments);
         }
+        public ActionResult Add()
+        {
+            
+            return View();
+
+        }
+        [HttpPost]
+         public string Add(Department department)
+        {
+            
+            return "Record Saved";
+
+        }  
     }
 }
--- EmployeeManagement/Models/Department.cs
+++ EmployeeManagement/Models/Department.cs
@@ -1,8 +1,11 @@
-namespace DepartmentManagement.Models
+using System.ComponentModel.DataAnnotations;
+namespace EmployeeManagement.Models
 {
     public class Department
     {
         public int Id {get; set;}
+
+        [Required(ErrorMessage="Department name is required.")]
         public string DepartmentName {get;  set;}
         public float Salary { get; set;}
     }
