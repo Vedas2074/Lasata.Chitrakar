560737079e30161b3cc68d282829fd1b3224972a
chitrakar <lasata16@gmail.com>
2020-12-14 19:24:48 +0545

    learned about file handling

--- CSharpBasics/Generic.cs
+++ CSharpBasics/Generic.cs
@@ -1,17 +1,10 @@
-// public class Calculator
-// {
-//     //public int static int Sum(int firstNum, int secondNum)
-//     {
-//         return firstNum + secondNum;
-//     }
+public class Calculator
+{
+    public static T Sum<T>(T firstNum, T secondNum)
+    {
+        return firstNum;
+    }
 
-//     public int static double Sum(double firstNum, double secondNum)
-//     {
-//         return firstNum + secondNum;
-//     }
 
-//     public static T Sum<T>(T  firstNum, T secondNum)
-//     {
-//         return firstNum;
-//     }
-// }
+    
+}
